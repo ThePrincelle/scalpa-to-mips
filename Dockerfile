9f924aff668fbc2113ce25131a52a59adf8d0a06
Maxime Princelle <maxime@princelle.org>
2020-12-22 23:46:18 +0100

    Add MIPS testing to tests

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,21 @@
 FROM ubuntu:latest
 
-RUN apt-get update -y \
-  && apt-get install -y --no-install-recommends build-essential bison flex \
-  && rm -rf /var/lib/apt/lists/*
+# Configure Timezone & tzdata
+ENV TZ=Europe/Paris
+RUN ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone
+RUN apt-get update -y
+RUN DEBIAN_FRONTEND="noninteractive" apt-get -y install tzdata
+RUN ln -fs /usr/share/zoneinfo/Europe/Paris /etc/localtime
+RUN dpkg-reconfigure --frontend noninteractive tzdata
 
+# Install requirements for testing
+RUN apt-get install -y --no-install-recommends build-essential bison flex default-jre
+
+# Clean image
+RUN rm -rf /var/lib/apt/lists/*
+
+# Copy project files
 COPY . /app
 
+# Setup working directory
 WORKDIR /app
--- test.sh
+++ test.sh
@@ -6,6 +6,8 @@
 FILE=$( realpath ${BASH_SOURCE[0]} )
 DIR=$(dirname -- "$FILE" )
 
+SCALPA_EXT=".pas"
+
 # Go to root of project
 cd ${DIR}
 
@@ -17,23 +19,46 @@ make
 echo ""
 echo "Running tests..."
 echo ""
-for f in "$DIR"/tests/*.pas
+for f in "$DIR"/tests/*"${SCALPA_EXT}"
 do
     # Get Relative path
     testfile=".${f#"$DIR"}"
 
-    echo "Running test for: ${testfile}..."
+    echo "- Running tests for: ${testfile}..."
+    echo ""
 
     # Run test
+    echo "  🔄  Running Scalpa to MIPS compiler on: ${testfile_mips}"
+    echo ""
+
     if ./a.out "${testfile}" ; then
-        echo "Test succeeded for: ${testfile}"
+        echo "  ✅  Compilation succeeded for: ${testfile}"
+        echo ""
+
+        testfile_mips="${testfile%"$SCALPA_EXT"}.s"
+
+
+        echo "  🔄  Running compiled MIPS code: ${testfile_mips}"
+        echo -e "\033[32m"
+
+        if java -jar Mars4_5.jar dec v1 "${testfile_mips}" ; then
+            echo -e "\033[0m"
+            echo "  ✅  MIPS code runned successfully for: ${testfile_mips}"
+        else
+            echo -e "\033[0m"
+            echo "🔥  Error for running compiled MIPS code on: ${testfile_mips}"
+            exit 1
+        fi
+
     else
-        echo "Test failed on: ${testfile}"
+        echo "🔥  Compilation failed for: ${testfile}"
         exit 1
     fi
 
     echo ""
+    echo "  ✅  Tests passed for: ${testfile}"
+    echo ""
 done
 
 # All tests have passed.
-echo "All tests passed successfully."
+echo "✅  All tests passed successfully."

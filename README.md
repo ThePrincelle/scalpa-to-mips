fa15856e63401dddf9d6715cbe0612470c5fc8e7
Maxime Princelle <maxime@princelle.org>
2020-12-22 22:38:23 +0100

    Add Tests

---
+++ README.md
@@ -0,0 +1,9 @@
+# Scalpa to MIPS
+
+[![Tests Status](https://img.shields.io/drone/build/lpolitanski/scalpa-to-mips?label=tests&server=https%3A%2F%2Fdrone.princelle.org)](https://drone.princelle.org/lpolitanski/scalpa-to-mips)
+
+Membres du groupe :
+
+- Hugo Brua
+- Louis Politanski
+- Maxime Princelle
---
+++ test.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+# test.sh
+# Testing scalpa to mips compiler
+# ----------------------------------------------------------------
+
+FILE=$( realpath ${BASH_SOURCE[0]} )
+DIR=$(dirname -- "$FILE" )
+
+# Go to root of project
+cd ${DIR}
+
+# Build the compiler
+echo "Building compiler..."
+make
+
+# Run tests
+echo ""
+echo "Running tests..."
+echo ""
+for f in "$DIR"/tests/*.pas
+do
+    # Get Relative path
+    testfile=".${f#"$DIR"}"
+
+    echo "Running test for: ${testfile}..."
+
+    # Run test
+    if ./a.out "${testfile}" ; then
+        echo "Test succeeded for: ${testfile}"
+    else
+        echo "Test failed on: ${testfile}"
+        exit 1
+    fi
+
+    echo ""
+done
+
+# All tests have passed.
+echo "All tests passed successfully."
